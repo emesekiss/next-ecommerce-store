7a86bb5dd162d0e9952ce120e69ede0d96611018
Emese Kiss <emesekiss92@yahoo.com>
2020-11-11 11:31:10 +0100

    Create a configuration file for Ley

--- util/database.js
+++ util/database.js
@@ -7,7 +7,13 @@ extractHerokuDatabaseEnvVars();
 
 dotenv.config();
 
-const sql = postgres();
+const sql =
+  process.env.NODE_ENV === 'production'
+    ? // Heroku needs SSL connections but
+      // has an "unauthorized" certificate
+      // https://devcenter.heroku.com/changelog-items/852
+      postgres({ ssl: { rejectUnauthorized: false } })
+    : postgres();
 
 export async function getProducts() {
   const products = await sql`
